f4b9f7d856d11a82d91f90a32995f490c50183fb
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2025-05-24 21:45:37 -0300

    login folder added

---
+++ login/shield.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require 'connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($_POST['email']) and !empty($_POST['senha'])) {
+
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $consulta = $connection->prepare('SELECT * FROM login WHERE email = :e');
+        $consulta-> bindValue(':e', $email);
+        $consulta->execute();
+        $usuario = $consulta->fetch();
+
+        if ($usuario) {
+            if ($senha === $usuario['senha']) { 
+                $_SESSION['email'] = $usuario['email'];
+                header('Location: loggedin.php');
+                exit;
+            } 
+            else {
+                echo '<h1>Usuário ou senha incorretos!</h1>';
+            }
+        }
+        else {
+            echo '<h1>Usuário ou senha incorretos!</h1>';
+        }
+    }
+    else {
+        echo '<h1>Preencha todos os campos!</h1>';
+    }
+}
+else {
+    header('Location: index.php');
+    exit;
+}
